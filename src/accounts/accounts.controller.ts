be685762e40b2443e4a7adc5e0f28ce320320053
Nikita Kornilov <krnl@feip.co>
2024-07-26 11:59:12 +0600

    Finished tutorial, add accounts module

---
+++ src/accounts/accounts.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  ParseIntPipe,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AccountsService } from './accounts.service';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+
+@Controller('accounts') // accounts
+export class AccountsController {
+  constructor(private readonly accountsService: AccountsService) {}
+
+  @Get() // GET /accounts /accounts?currency=rub
+  findAll(@Query('role') role?: string) {
+    return this.accountsService.findAll();
+  }
+
+  @Get(':id') // GET /accounts/:id or
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.accountsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body(ValidationPipe) account: CreateAccountDto) {
+    return this.accountsService.create(account);
+  }
+
+  @Get(':id') // PATCH /accounts/:id
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) userUpdate: UpdateAccountDto,
+  ) {
+    return this.accountsService.update(id, userUpdate);
+  }
+
+  @Delete(':id') // DELETE /accounts
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.accountsService.delete(id);
+  }
+}
---
+++ src/accounts/accounts.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateAccountDto } from './dto/update-account.dto';
+import { CreateAccountDto } from './dto/create-account.dto';
+
+@Injectable()
+export class AccountsService {
+  private accounts = [
+    {
+      id: 1,
+      name: '1',
+    },
+    {
+      id: 2,
+      name: '2',
+    },
+  ];
+
+  findAll() {
+    return this.accounts;
+  }
+
+  findOne(id: number) {
+    const user = this.accounts.find((account) => account.id === id);
+
+    if (user === undefined) throw new NotFoundException('User Not Found');
+
+    return user;
+  }
+
+  create(createAccountDto: CreateAccountDto) {
+    const accountsByHighestId = [...this.accounts].sort((a, b) => b.id - a.id);
+    const newAccount = {
+      id: accountsByHighestId[0].id + 1,
+      ...createAccountDto,
+    };
+    this.accounts.push(newAccount);
+    return newAccount;
+  }
+
+  update(id: number, updatedAccountDto: UpdateAccountDto) {
+    this.accounts.map((account) => {
+      if (account.id == id) {
+        return { ...account, ...updatedAccountDto };
+      }
+      return account;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.accounts = this.accounts.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
